0505d6ce5cf05f688c693a7e217b775352599e61
anonis2107 <Anne.Nonis@student.unimelb.edu.au>
2026-05-06 01:56:01 +1000

    Add products to JS for product page template data

---
+++ scripts.js
@@ -0,0 +1,142 @@
+const products = {
+    //Cards & Stationary
+    bookmark: {
+        name: "Bookmarks",
+        price: $50,
+        image: "bookmarks.png"
+    },
+    envelope: {
+    name: "Envelopes",
+    price: 50,
+    image: "envelopes.png"
+    },
+    invitation: {
+        name: "Invitation Cards",
+        price: 50,
+        image: "invitations.png"
+    },
+    postcard: {
+        name: "Postcards",
+        price: 50,
+        image: "postcards.png"
+    },
+    notepad: {
+        name: "Notepads",
+        price: 50,
+        image: "notepads.png"
+    },
+
+    //CD & Packaging
+    cdcover: {
+        name: "CD Covers",
+        price: 50,
+        image: "cdcovers.png"
+    },
+
+    //Custom Stickers
+    diecutstickers: {
+        name: "Die Cut Stickers",
+        price: 50,
+        image: "diecut.png"
+    },
+
+    clearstickers: {
+        name: "Clear Stickers",
+        price: 50,
+        image: "clearsticker.png"
+    },
+
+    embossedstickers: {
+        name: "Embossed Stickers",
+        price: 50,
+        image: "embossedstickers.png"
+    },
+
+    heavydutystickers: {
+        name: "Heavy Duty Stickers",
+        price: 50,
+        image: "heavyduty.png"
+    },
+
+    //Gift card
+
+    giftcards: {
+        name: "Gift Cards",
+        price: 50,
+        image: "giftcards.png"
+    },
+
+    //Label & Tags
+
+    assetlabels: {
+        name: "Asset Labels",
+        price: 50,
+        image: "assetlabels.png"
+    },
+
+    doorhangers: {
+        name: "Door Hangers",
+        price: 50,
+        image: "doorhangers.png"
+    },
+
+    standardcustomlabels: {
+        name: "Standard Custom Labels",
+        price: 50,
+        image: "standardcustomlabels.png"
+    },
+
+    //Large Sinage
+
+    posters: {
+        name: "Posters",
+        price: 50,
+        image: "posters.png"
+    },
+
+    vinybanner: {
+        name: "Vinyl Banner",
+        price: 50,
+        image: "vinylbanner.png"
+    },
+
+    // Marketing Materials
+
+    businesscards: {
+        name: "Business Cards",
+        price: 50,
+        image: "businesscards.png"
+    },
+
+    brochures: {
+        name: "Brochures",
+        price: 50,
+        image: "brochures.png"
+    },
+
+    eventtickets: {
+        name: "Event Tickets",
+        price: 50,
+        image: "eventtickets.png"
+    },
+
+    menus: {
+        name: "Menus",
+        price: 50,
+        image: "menus.png"
+    },
+
+    tabletents: {
+        name: "Table Tents",
+        price: 50,
+        image: "tabletents.png"
+    },
+
+    //Speciality Finishes
+    foilstamping: {
+        name: "Foil Stamping",
+        price: 50,
+        image: "foilstamping.png"
+    }
+
+};
